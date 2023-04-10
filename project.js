057bd8dbcdefb1c1fdc3ed4ed1772dda6f3b700b
Fredrick <mwangikingz94@gmail.com>
2023-04-10 14:57:03 +0300

    change files

--- project.js
+++ project.js
@@ -1,39 +1,32 @@
-// fetch("http://localhost:3000/MRData")
-// .then(res => res.json())
-// .then(data => console.log(data))
- 
-// fetch("http://localhost:3000/MRData"),then((data)=>{
-//     //console.log(data)
-//     return data.json();
-// }).then((completedata)=>{
-//     console.log(completedata[2].title)
-//     document.getElementById('root').
-//     innerHTML=completedata[2].title;
-// });
-const searchBar = document.getElementById('search-btn')
-const mysearchBtn = document.getElementById('root');
-const driverList = document.getElementById('container');
+// 
 
-searchBar.addEventListener('click', getDriver);
 
-function getDriver(){
-  let finding = document.getElementById('search-btn').value;
-  fetch('http://ergast.com/api/f1/drivers.json')
-    .then(response => response.json())
-    .then(data => {
-        let myHtml = "";
-      // loop through each driver and create a card for them
-      data.Drivers.forEach(driver => {
-        myHtml +=`
-        const card = document.createElement('div');
-        card.classList.add('card');
-        card.innerHTML = 
-          <div class="card-details">
-            <h3>${driver.driverId}</h3>
-            <h4>${driver.url}</h4>
-          </div>
-        `;
+
+
+function getDriver() {
+    let finding = document.getElementById('search-btn').value;
+    let driverList = document.getElementById('driver-list');
+    driverList.innerHTML = ""; // clear the previous search results
+    let myHtml = "";
+  
+    fetch(`http://ergast.com/api/f1/drivers/${finding}.json`)
+      .then(response => response.json())
+      .then(data => {
+        // loop through each driver and create a card for them
+        data.drivers.forEach(driver => {
+          myHtml += `
+            <div class="card">
+              <div class="card-details">
+                <h3>${driver.driverId}</h3>
+                <h4>${driver.url}</h4>
+              </div>
+            </div>
+          `;
+        });
+        driverList.innerHTML = myHtml;
+      })
+      .catch(error => {
+        console.error(error);
+        driverList.innerHTML = "Error fetching data. Please try again.";
       });
-      driverList.innerHTML+= myHtml;
-    });
-}
+  }
