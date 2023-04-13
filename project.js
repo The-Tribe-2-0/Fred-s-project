1162749840b32a8b574527ddc5a3bd756ac771ab
Fredrick <mwangikingz94@gmail.com>
2023-04-13 20:07:22 +0300

    review

--- project.js
+++ project.js
@@ -1,85 +1,66 @@
-// fetch("http://localhost:3000/MRData")
-// .then(res => res.json())
-// .then(data => console.log(data))
- 
-// fetch("http://localhost:3000/MRData"),then((data)=>{
-//     //console.log(data)
-//     return data.json();
-// }).then((completedata)=>{
-//     console.log(completedata[2].title)
-//     document.getElementById('root').
-//     innerHTML=completedata[2].title;
-// });
 
+// function getDriver() {
+//     let finding = document.getElementById('search-btn').addEventListener("click");
+//     let driverList = document.getElementById('driver-list');
+//     driverList.innerHTML = ""; // clear the previous search results
+//     let myHtml = "";
+  
+//     fetch(`http://ergast.com/api/f1/drivers/${finding}.json`)
+//       .then(response => response.json())
+//       .then(data => {
+//         // loop through each driver and create a card for them
+//         data.drivers.forEach(driver => {
+//           myHtml += `
+//             <div class="card">
+//               <div class="card-details">
+//                 <h3>${driver.driverId}</h3>
+//                 <h4>${driver.url}</h4>
+//               </div>
+//             </div>
+//           `;
+//         });
+//         driverList.innerHTML = myHtml;
+//       })
+//       .catch(error => {
+//         console.error(error);
+//         driverList.innerHTML = "Error fetching data. Please try again.";
+//       });
+//   }
 
-// const searchBar = document.getElementById('search-bar');
-// const searchBtn = document.getElementById('search-btn');
-// const driverList = document.getElementById('driver-list');
+// const searchBtn = document.getElementById("search-btn");
 
-// searchBtn.addEventListener('click', getDrivers);
+// searchBtn.addEventListener("click", getDriver);
 
-// function getDrivers() {
-//   let searchQuery = searchBar.value;
-//   fetch(`http://ergast.com/api/f1/${searchQuery}/drivers.json`)
+// function getDriver() {
+//   fetch("http://ergast.com/api/f1/drivers.json")
 //     .then(response => response.json())
 //     .then(data => {
-//       let driversHtml = "";
-//       data.MRData.DriverTable.Drivers.forEach(driver => {
-//         driversHtml += `
+//       let html = "";
+//       data.forEach(driver => {
+//         html += `
 //           <div class="card">
-//             <h3>${driver.givenName} ${driver.familyName}</h3>
-//             <p>Nationality: ${driver.nationality}</p>
-//             <a href="${driver.url}">Click Here</a>
+//             <div class="card-details">
+//               <h3>${driver.driverId}</h3>
+//               <h4>${driver.url}</h4>
+//             </div>
 //           </div>
 //         `;
 //       });
-//       driverList.innerHTML = driversHtml;
+//       document.getElementById("search-results").innerHTML = html;
 //     })
 //     .catch(error => {
 //       console.error(error);
-//       driverList.innerHTML = "<p>Failed to retrieve drivers</p>";
 //     });
 // }
 
 
-// const searchBtn = document.getElementById("search-btn");
-// const searchInput = document.getElementById("search-bar");
-// const driverList = document.getElementById("driver-list");
-
-// searchBtn.addEventListener("click", function() {
-//   const searchText = searchInput.value;
-//   let searchQuery = searchText;
-//   fetch(`http://ergast.com/api/f1/${searchQuery}/drivers.json`)
-//     .then(response => response.json())
-//     .then(data => {
-//       let driversHtml = "";
-//       data.MRData.DriverTable.Drivers.forEach(driver => {
-//         driversHtml += `
-//           <div class="card">
-//             <h3>${driver.givenName} ${driver.familyName}</h3>
-//             <p>Nationality: ${driver.nationality}</p>
-//             <a href="${driver.url}">Click Here</a>
-//           </div>
-//         `;
-//       });
-//       driverList.innerHTML = driversHtml;
-//     })
-//     .catch(error => {
-//       console.error(error);
-//       driverList.innerHTML = "<p>Failed to retrieve drivers</p>";
-//     });
-// });
-
-  // Do something with searchText, such as search a database or perform a web search
-
-
   
-  const searchForm = document.getElementById('search-form');
+const searchForm = document.getElementById('search-form');
 const searchInput = document.getElementById('search-input');
 const searchResults = document.getElementById('search-results');
 const searchBtn = document.getElementById("searchBtn");
 
-searchForm.addEventListener('submit', (event) => {
+searchForm.addEventListener('click', (event) => {
   event.preventDefault(); // prevent form submission
 
   const searchTerm = searchInput.value;
